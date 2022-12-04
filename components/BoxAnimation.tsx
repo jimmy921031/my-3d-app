a32e4424e446e922a34525850f5cd1e5cd22d0af
jimmy921031 <98251543+jimmy921031@users.noreply.github.com>
2022-12-04 18:59:27 +0800

    chapter 3

--- components/BoxAnimation.tsx
+++ components/BoxAnimation.tsx
@@ -1,12 +1,15 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { useHelper } from "@react-three/drei";
+import { BoxHelper } from "three";
 
 const BoxAnimation = () => {
   const meshRef = useRef<THREE.Mesh>(null);
+  useHelper(meshRef, BoxHelper, "blue");
   useFrame(() => {
     console.log("hi");
     if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.x += 0.02;
     }
   });
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import BoxAnimation from "../components/BoxAnimation";
 import CameraOrbitController from "../components/CameraOrbitController";
+import { OrbitControls, Stats } from "@react-three/drei";
 
 export default function Home() {
+  const testing = true;
+
   return (
     <div className="container">
       <Canvas>
-        <CameraOrbitController />
+        {testing ? <axesHelper args={[5]} /> : null}
+        {testing ? <Stats /> : null}
+        {testing ? <gridHelper args={[10, 10]} /> : null}
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <BoxAnimation />
