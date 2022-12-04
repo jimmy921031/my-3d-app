3df36f5de5b756db5d99e8e6582956edfff21962
jimmy921031 <98251543+jimmy921031@users.noreply.github.com>
2022-12-04 19:17:56 +0800

    chapter 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import BoxAnimation from "../components/BoxAnimation";
 import CameraOrbitController from "../components/CameraOrbitController";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { OrbitControls, Stats, PerspectiveCamera } from "@react-three/drei";
 
 export default function Home() {
   const testing = true;
@@ -9,9 +9,10 @@ export default function Home() {
   return (
     <div className="container">
       <Canvas>
+        <PerspectiveCamera args={[45, 1, 1, 500]} />
         {testing ? <axesHelper args={[5]} /> : null}
         {testing ? <Stats /> : null}
-        {testing ? <gridHelper args={[10, 10]} /> : null}
+        {testing ? <gridHelper args={[100, 100]} /> : null}
         <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
